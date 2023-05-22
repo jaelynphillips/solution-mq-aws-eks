5e8f4d40d13779ab29626f09429ec7ecb845b7c7
Berret Terry <berretterry@ibm.com>
2023-05-22 12:02:24 -0700

    docusaurus build and initial upload

---
+++ website/variables.js
@@ -0,0 +1,8 @@
+const Info = {
+    repo: 'https://github.com/berretterry-ibm/solution-mq-aws-eks.git',
+    project: 'solution-mq-aws-eks',
+    org: 'berretterry-ibm',
+    url: 'https://berretterry-ibm.github.io',
+    baseUrl: '/solution-mq-aws-eks/'
+};
+module.exports = { Info };
